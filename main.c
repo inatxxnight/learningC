e341462c2d245eb23a209979848753c6e61a1615
SkyAngel_Off <lucas.lamothe@outlook.fr>
2019-02-10 17:24:45 +0100

    Create a MainMenu
    This menu is for choosing a second menu thus a second function.

--- main.c
+++ main.c
@@ -4,6 +4,33 @@
 int main()
 {
   int choice = 0;
+  printf("=== Fonctions ===\n");
+  printf("1. Restaurant\n");
+  printf("2. Conditions\n");
+  printf("Votre Choix ?\n");
+  scanf("%d",&choice);
+
+  switch(choice)
+  {
+  case 1:
+    printf("\nVous avez choisi la Fonction : Restaurant\n");
+    restaurant();
+    break;
+  case 2:
+    printf("\nVous avez choisi la Fonction : Conditions\n");
+    conditions();
+    break;
+  default:
+    printf("Choix invalable\n");
+    break;
+  }
+
+  return 0;
+}
+
+int restaurant()
+{
+  int choice = 0;
   printf("=== Menu ===\n");
   printf("1. Royal Cheese\n");
   printf("2. Mc Deluxe\n");
@@ -30,5 +57,14 @@ int main()
     printf("\nVotre choix est incorret. Vous ne mangerez donc rien!\n");
     break;
   }
+
+  return 0;
+}
+
+int conditions()
+{
+  int age = 3;
+  int majeur = (age >= 18) ? 18 : 0;
+  printf("Majeur : %d", majeur);
   return 0;
 }
