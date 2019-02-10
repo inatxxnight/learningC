29123f2819ddbb6215438b0335a4a16219886c61
SkyAngel_Off <lucas.lamothe@outlook.fr>
2019-02-10 18:35:53 +0100

    Create 'Plus ou Moins'
    New Update : Creating my first console game, 'Plus ou Moins' :smile:

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
@@ -7,6 +8,7 @@ int main()
   printf("=== Fonctions ===\n");
   printf("1. Restaurant\n");
   printf("2. Conditions\n");
+  printf("3. Jeu 'Plus ou Moins'\n");
   printf("Votre Choix ?\n");
   scanf("%d",&choice);
 
@@ -19,6 +21,10 @@ int main()
   case 2:
     printf("\nVous avez choisi la Fonction : Conditions\n");
     conditions();
+    break;
+  case 3:
+    printf("\nVous avez choisi la Fonction : Jeu 'Plus ou Moins'\n");
+    firstgame();
     break;
   default:
     printf("Choix invalable\n");
@@ -66,5 +72,36 @@ int conditions()
   int age = 3;
   int majeur = (age >= 18) ? 18 : 0;
   printf("Majeur : %d", majeur);
+  return 0;
+}
+
+int firstgame()
+{
+  srand(time(NULL));
+  const int MAX = 100, MIN = 1;
+  int nombreMystere = 0, mynumber = 0, compteur = 0;
+  nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+  printf("Le Nombre mystere est entre 1 et 100\n");
+
+  do
+  {
+    printf("Quel est le nombre mystere ? ");
+    scanf("%d", &mynumber);
+    compteur++;
+    if(mynumber > nombreMystere)
+    {
+      printf("C'est moins !\n");
+    }
+    else if(mynumber < nombreMystere)
+    {
+      printf("C'est plus !\n");
+    }
+    else
+    {
+      printf("\nVous avez gagne bien joue !\n");
+      printf("Vous avez gagne en %d coups\n", compteur);
+    }
+  }while(mynumber != nombreMystere);
+
   return 0;
 }
