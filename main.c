808f291666e4985f075e53e0695e2413d4122453
SkyAngel_Off <lucas.lamothe@outlook.fr>
2019-02-10 16:54:45 +0100

    Create a Menu
    Create a console menu with the switch (conditions).

--- main.c
+++ main.c
@@ -3,18 +3,32 @@
 
 int main()
 {
-  int A = 0;
-  printf("Choisir un nombre ?\n");
-  scanf("%d", &A);
-  printf("Choisir un diviseur de %d ?\n", A);
-  int B = 0;
-  scanf("%d", &B);
-  printf("\nDivision euclidienne de %d par %d :\n", A,B);
-  int quotient = 0;
-  quotient = A/B;
-  printf("Quotient : 0%d\n", quotient);
-  int reste = 0;
-  reste = A%B;
-  printf("Reste : %d\n", reste);
+  int choice = 0;
+  printf("=== Menu ===\n");
+  printf("1. Royal Cheese\n");
+  printf("2. Mc Deluxe\n");
+  printf("3. Mc Bacon\n");
+  printf("4. Big Max\n");
+  printf("Votre Choix ?\n");
+  scanf("%d",&choice);
+
+  switch(choice)
+  {
+  case 1:
+    printf("\nVous avez choisi le Royal Cheese\n");
+    break;
+  case 2:
+    printf("\nVous avez choisi le Mc Deluxe\n");
+    break;
+  case 3:
+    printf("\nVous avez choisi le Mc Bacon\n");
+    break;
+  case 4:
+    printf("\nVous avez choisi le Big Mac\n");
+    break;
+  default:
+    printf("\nVotre choix est incorret. Vous ne mangerez donc rien!\n");
+    break;
+  }
   return 0;
 }
