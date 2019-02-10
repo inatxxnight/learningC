c9718656a3998c29335334278ecd834d98b12d43
SkyAngel_Off <lucas.lamothe@outlook.fr>
2019-02-10 19:09:31 +0100

    Update of my Game
    The game comports 3 levels different and two modes, with 1 player or 2 players.

--- main.c
+++ main.c
@@ -28,6 +28,7 @@ int main()
     break;
   default:
     printf("Choix invalable\n");
+    main();
     break;
   }
 
@@ -61,6 +62,7 @@ int restaurant()
     break;
   default:
     printf("\nVotre choix est incorret. Vous ne mangerez donc rien!\n");
+    restaurant();
     break;
   }
 
@@ -77,31 +79,102 @@ int conditions()
 
 int firstgame()
 {
+  int choice = 0, nombreMystere = 0, mynumber = 0, compteur = 0, MAX = 100, MIN = 1;
   srand(time(NULL));
-  const int MAX = 100, MIN = 1;
-  int nombreMystere = 0, mynumber = 0, compteur = 0;
-  nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
-  printf("Le Nombre mystere est entre 1 et 100\n");
+  printf("=== Niveau de Difficulte ===\n");
+  printf("1. Normal\n");
+  printf("2. Avance\n");
+  printf("3. Expert\n");
+  printf("Votre Choix ?\n");
+  scanf("%d",&choice);
 
-  do
+  switch(choice)
   {
-    printf("Quel est le nombre mystere ? ");
-    scanf("%d", &mynumber);
-    compteur++;
-    if(mynumber > nombreMystere)
+  case 1:
+    printf("\nVous avez choisi le Niveau : Normal\n");
+    MAX = 100, MIN = 1;
+    break;
+  case 2:
+    printf("\nVous avez choisi le Niveau : Avance\n");
+    MAX = 1000, MIN = 1;
+    break;
+  case 3:
+    printf("\nVous avez choisi le Niveau : Expert\n");
+    MAX = 10000, MIN = 1;
+    break;
+  default:
+    printf("\nVotre choix est incorret\n");
+    main();
+    break;
+  }
+
+  int choice2 = 0;
+  srand(time(NULL));
+  printf("=== Nombre de Joueurs ===\n");
+  printf("1. 1 Joueur\n");
+  printf("2. 2 Joueurs\n");
+  printf("Votre Choix ?\n");
+  scanf("%d",&choice2);
+
+  switch(choice2)
+  {
+  case 1:
+    printf("\nVous avez choisi le mode : 1 Joueur\n");
+    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+    printf("Le Nombre mystere est entre %d et %d\n", MIN, MAX);
+
+    do
     {
-      printf("C'est moins !\n");
-    }
-    else if(mynumber < nombreMystere)
+        printf("Quel est le nombre mystere ? ");
+        scanf("%d", &mynumber);
+        compteur++;
+        if(mynumber > nombreMystere)
+        {
+            printf("C'est moins !\n");
+        }
+        else if(mynumber < nombreMystere)
+        {
+            printf("C'est plus !\n");
+        }
+        else
+        {
+            printf("\nVous avez gagne bien joue !\n");
+            printf("Vous avez gagne en %d coups\n", compteur);
+        }
+    }while(mynumber != nombreMystere);
+    break;
+  case 2:
+    printf("\nVous avez choisi le mode : 2 Joueurs\n");
+    printf("Joueur 1 : Definissez le Nombre Mystere\n");
+    scanf("%d", &nombreMystere);
+    printf("\r\r**");
+    printf("Joueur 2 : Le Nombre mystere est entre %d et %d\n", MIN, MAX);
+
+    do
     {
-      printf("C'est plus !\n");
-    }
-    else
-    {
-      printf("\nVous avez gagne bien joue !\n");
-      printf("Vous avez gagne en %d coups\n", compteur);
-    }
-  }while(mynumber != nombreMystere);
+        printf("Joueur 2 : Quel est le nombre mystere ? ");
+        scanf("%d", &mynumber);
+        compteur++;
+        if(mynumber > nombreMystere)
+        {
+            printf("C'est moins !\n");
+        }
+        else if(mynumber < nombreMystere)
+        {
+            printf("C'est plus !\n");
+        }
+        else
+        {
+            printf("\nJoueur 2 : Vous avez gagne bien joue !\n");
+            printf("Joueur 2 : Vous avez gagne en %d coups\n", compteur);
+        }
+    }while(mynumber != nombreMystere);
+    break;
+  default:
+    printf("\nVotre choix est incorret\n");
+    main();
+    break;
+  }
 
   return 0;
 }
