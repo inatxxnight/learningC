28201ca3565eb42230be57fb276826ec2e659afd
SkyAngel_Off <lucas.lamothe@outlook.fr>
2019-02-10 14:21:12 +0100

    Use of variables
    In this commit, I have used variables whith the native function printf()

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 
 int main()
 {
-    printf("Hello World!");
+    int day = 10;
+    int month = 0;
+    int month2 = 2;
+    int years = 2019;
+    int myAge = 17;
+    printf("J'ai actuellement le %d/%d%d/%d : %d ans\n", day,month,month2,years,myAge);
     return 0;
 }
